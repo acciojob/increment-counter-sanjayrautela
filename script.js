ce0a1dc945ac82d884ee1ebcb08302f6a122b431
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-09 13:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const counterElement = document.getElementById('counter');
+const incrementButton = document.getElementByID('incrementBtn');
+
+let counterValue = 0;
+
+function incrementCounter(){
+	alert(`counter value before increment: ${counterValue}`);
+
+	countValue++;
+	counterElement.textContent = countValue;
+}
+
+incrementButton.addEventListener('click' , incrementCounter);
